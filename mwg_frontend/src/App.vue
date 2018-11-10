34bacd6b4bee26e928f64c8fff25cc32beb6588e
Dawid_g510 <227970@student.pwr.edu.pl>
2018-11-10 15:02:10 +0100

    init project

---
+++ mwg_frontend/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-app>
+    <v-toolbar>
+      <v-toolbar-side-icon @click.native.stop="showSideNav =!showSideNav"></v-toolbar-side-icon>
+      <v-toolbar-title>Toolbar title</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items>
+        <v-btn flat>
+          <v-icon left class="primary">supervisor_account</v-icon>
+          Wyloguj
+        </v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+    <v-navigation-drawer v-model="showSideNav">
+      <v-list>
+        <v-list-tile>
+          <v-list-tile-action>
+            <v-icon>supervisor_account</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-conetent>
+            Cos tam
+          </v-list-tile-conetent>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <main>
+      <router-view></router-view>
+    </main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      showSideNav: false
+    }
+  }
+}
+</script>
---
+++ mwg_frontend/src/store/store.js
@@ -0,0 +1,14 @@
+import Vue from 'vue'
+import Vuex from 'vuex';
+
+import actions from './actions'
+import state from './state'
+import mutations from './mutations'
+import getters from './getters'
+
+export const store = new Vuex.Store({
+  state,
+  actions,
+  mutations,
+  getters
+})
